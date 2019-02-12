64f1e143b372e2985d237e70e356df37143dbb33
Kelly McGuire <kellyleecemcguire@gmail.com>
2019-02-12 16:02:37 -0500

    about to switch around the use of getcstraces to be able to add types of traces to t2p. saving as a checkpoint

--- cascade/utils.py
+++ cascade/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def getcstraces(run, cs='', trace_type='zscore_iti', start_time=-1, end_time=6,
-                downsample=True, nan_artifacts=True, thresh=8,
+                downsample=True, clean_artifacts=None, thresh=10,
                 warp=False):
     """
     Wrapper function for flow.Trace2P.cstraces() or .warpsctraces().
@@ -14,12 +14,17 @@ def getcstraces(run, cs='', trace_type='zscore_iti', start_time=-1, end_time=6,
     Parameters
     ----------
     run : Run object
+    cs : str
+        Type of CS. e.g., plus, minus, neutral, 0, 135, 270, ...
     trace_type : str
         dff, zscore, zscore_iti, deconvolved
     downsample : bool
         Downsample from 31 to 15 Hz sampling rate
-    nan_artifacts : bool
-        Romove huge artifacts in dff traces by interpolating
+    clean_artifacts : str
+        nan, interp; Remove huge artifacts in dff traces by interpolating
+        or adding in nan values
+        Note: setting either value here will cause zscoring to nan artifacts
+        before calculating mu/sigma
     thresh : int
         Threshold for removing artifacts
     warp_ : bool
@@ -38,6 +43,7 @@ def getcstraces(run, cs='', trace_type='zscore_iti', start_time=-1, end_time=6,
 
     # trigger all trials around stimulus onsets
     if warp:
+        print('warping')
         run_traces = t2p.warpcstraces(cs, start_s=start_time, end_s=end_time,
                                       trace_type='dff', cutoff_before_lick_ms=-1,
                                       errortrials=-1, baseline=(-1, 0),
@@ -48,31 +54,49 @@ def getcstraces(run, cs='', trace_type='zscore_iti', start_time=-1, end_time=6,
                                   errortrials=-1, baseline=(-1, 0),
                                   baseline_to_stimulus=True)
 
-    # z-score
-    if 'zscore' in trace_type:
-        if trace_type == 'zscore':
-            mu = pool.calc.zscore.mu(date, nan_artifacts=nan_artifacts,
-                                     thresh=thresh)
-            sigma = pool.calc.zscore.sigma(date, nan_artifacts=nan_artifacts,
-                                           thresh=thresh)
-        elif trace_type == 'zscore_iti':
-            mu = pool.calc.zscore.iti_mu(date, nan_artifacts=nan_artifacts)
-            sigma = pool.calc.zscore.iti_sigma(date, nan_artifacts=nan_artifacts)
-        sz = np.shape(run_traces)  # dims: (cells, time, trials)
-        run_traces = ((((run_traces
-                      .reshape((sz[0], sz[1]*sz[2])).T - mu)/sigma).T)
-                      .reshape((sz[0], sz[1], sz[2])))
-
-    # nan artifacts over a given threshold, dialate around threshold crossings
-    if nan_artifacts:
+    # clean artifacts
+    if clean_artifacts:
         sz = np.shape(run_traces)  # dims: (cells, time, trials)
         run_traces = run_traces.reshape((sz[0], sz[1]*sz[2]))
         nanpad = np.zeros(np.shape(run_traces))
         nanpad[np.abs(run_traces) > thresh] = 1
+        # dialate around threshold crossings
         for cell in range(sz[0]):
             nanpad[cell, :] = np.convolve(nanpad[cell, :], np.ones(3), mode='same')
-        run_traces[nanpad != 0] = np.nan
+        # clear with nans or interpolation
+        if clean_artifacts.lower() == 'nan':
+            print('cleaning with nan')
+            run_traces[nanpad != 0] = np.nan
+        elif clean_artifacts.lower() == 'interp':
+            print('cleaning with nan')
+            x = np.arange(0, sz[1]*sz[2])
+            for cell in range(sz[0]):
+                # x = np.where(np.isfinite(run_traces[cell, :]))[0]
+                if np.nansum(nanpad[cell, :]) > 0:
+                    blank = np.where(nanpad[cell, :] != 0)[0]
+                    keep = np.where(nanpad[cell, :] == 0)[0]
+                    run_traces[cell, blank] = np.interp(x[blank], x[keep],
+                                                        run_traces[cell, keep])
         run_traces = run_traces.reshape((sz[0], sz[1], sz[2]))
+
+    # z-score
+    if 'zscore' in trace_type:
+        sz = np.shape(run_traces)  # dims: (cells, time, trials)
+        arti = False if clean_artifacts is None else True
+        if trace_type == 'zscore':
+            print('zscoring')
+            mu = pool.calc.zscore.mu(date, nan_artifacts=arti,
+                                     thresh=thresh)
+            sigma = pool.calc.zscore.sigma(date, nan_artifacts=arti,
+                                           thresh=thresh)
+        elif trace_type == 'zscore_iti':
+            print('zscoring_iti')
+            mu = pool.calc.zscore.iti_mu(date, nan_artifacts=arti)
+            sigma = pool.calc.zscore.iti_sigma(date, nan_artifacts=arti)
+        run_traces = ((((run_traces
+                      .reshape((sz[0], sz[1]*sz[2])).T - mu)/sigma).T)
+                      .reshape((sz[0], sz[1], sz[2])))
+
 
     # downsample all traces/timestamps to 15Hz if framerate is 31Hz
     if (t2p.d['framerate'] > 30) and downsample:
