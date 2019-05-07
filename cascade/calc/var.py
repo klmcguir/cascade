c7cd9c8a66d671fe0c6856f2e7256a3b6d487399
Kelly McGuire <kellyleecemcguire@gmail.com>
2019-05-07 17:13:52 -0400

    trying with disk backend again

--- cascade/calc/var.py
+++ cascade/calc/var.py
@@ -7,7 +7,7 @@ import pandas as pd
 import os
 
 
-@memoize(across='mouse', updated=190429, returns='other', large_output=False)
+@memoize(across='mouse', updated=190507, returns='other', large_output=True)
 def groupday_varex_byday(
         mouse,
         trace_type='zscore_day',
@@ -121,7 +121,7 @@ def groupday_varex_byday(
     return dfvar
 
 
-@memoize(across='mouse', updated=190429, returns='other', large_output=False)
+@memoize(across='mouse', updated=190507, returns='other', large_output=True)
 def groupday_varex_byday_bycomp(
         mouse,
         trace_type='zscore_day',
@@ -225,7 +225,7 @@ def groupday_varex_byday_bycomp(
     return dfvar
 
 
-@memoize(across='mouse', updated=190429, returns='other', large_output=False)
+@memoize(across='mouse', updated=190507, returns='other', large_output=True)
 def groupday_varex_byday_bycomp_bycell(
         mouse,
         trace_type='zscore_day',
