41f88dfa86dd7a437c06c04b8359bfb7866ecfaf
Kelly McGuire <kellyleecemcguire@gmail.com>
2019-05-15 18:35:03 -0400

    db

--- cascade/cluster.py
+++ cascade/cluster.py
@@ -1103,7 +1103,7 @@ def trial_factors_summary_across_mice_days(
         df_list_error.append(pd.concat(df_mouse_error, axis=0))
         df_list_runmod.append(pd.concat(df_mouse_runmod, axis=0))
         df_list_ramp.append(pd.concat(df_mouse_ramp, axis=0))
-        df_list_ramp.append(pd.concat(df_mouse_ramp, axis=0))
+        df_list_fano.append(pd.concat(df_mouse_fano, axis=0))
 
     # concatenate all mice/runs together in final dataframe
     all_tempo_df = pd.concat(df_list_tempo, axis=0)
