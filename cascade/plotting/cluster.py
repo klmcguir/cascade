1a45862c55d87f8f7ea69c78b38ae1fb7b6b3fe6
Kelly McGuire <kellyleecemcguire@gmail.com>
2019-05-20 09:55:12 -0400

    new subclass of seaborn clustergrid with added functionality

--- cascade/plotting/cluster.py
+++ cascade/plotting/cluster.py
@@ -17,6 +17,257 @@ from .. import utils
 from .. import cluster
 import warnings
 
+# added functionality from https://github.com/mwaskom/seaborn/pull/1393/files
+class MyCluserGrid(sns.matrix.Clustergrid):
+    def __init__(self, data, pivot_kws=None, z_score=None, standard_scale=None,
+                 figsize=None, row_colors=None, col_colors=None, mask=None,
+                 expected_size_dendrogram=1.0,
+                 expected_size_colors=0.25):
+        """Grid object for organizing clustered heatmap input on to axes"""
+
+        if isinstance(data, pd.DataFrame):
+            self.data = data
+        else:
+            self.data = pd.DataFrame(data)
+
+        self.data2d = self.format_data(self.data, pivot_kws, z_score,
+                                       standard_scale)
+
+        self.mask = _matrix_mask(self.data2d, mask)
+
+        if figsize is None:
+            width, height = 10, 10
+            figsize = (width, height)
+        self.fig = plt.figure(figsize=figsize)
+
+        self.expected_size_dendrogram = expected_size_dendrogram
+        self.expected_size_side_colors = expected_size_colors
+
+        self.row_colors, self.row_color_labels = \
+            self._preprocess_colors(data, row_colors, axis=0)
+        self.col_colors, self.col_color_labels = \
+            self._preprocess_colors(data, col_colors, axis=1)
+
+        width_ratios = self.dim_ratios(self.row_colors,
+                                       figsize=figsize,
+                                       axis=0)
+
+        height_ratios = self.dim_ratios(self.col_colors,
+                                        figsize=figsize,
+                                        axis=1)
+        nrows = 3 if self.col_colors is None else 4
+        ncols = 3 if self.row_colors is None else 4
+
+        self.gs = gridspec.GridSpec(nrows, ncols, wspace=0.01, hspace=0.01,
+                                    width_ratios=width_ratios,
+                                    height_ratios=height_ratios)
+
+        self.ax_row_dendrogram = self.fig.add_subplot(self.gs[nrows - 1, 0:2])
+        self.ax_col_dendrogram = self.fig.add_subplot(self.gs[0:2, ncols - 1])
+        self.ax_row_dendrogram.set_axis_off()
+        self.ax_col_dendrogram.set_axis_off()
+
+        self.ax_row_colors = None
+        self.ax_col_colors = None
+
+        if self.row_colors is not None:
+            self.ax_row_colors = self.fig.add_subplot(
+                self.gs[nrows - 1, ncols - 2])
+        if self.col_colors is not None:
+            self.ax_col_colors = self.fig.add_subplot(
+                self.gs[nrows - 2, ncols - 1])
+
+        self.ax_heatmap = self.fig.add_subplot(self.gs[nrows - 1, ncols - 1])
+
+        # colorbar for scale to left corner
+
+        if self.col_colors is not None:
+            cbar_max = 3
+        else:
+            cbar_max = 2
+
+        self.cax = self.fig.add_subplot(self.gs[0:cbar_max, 0])
+
+        self.dendrogram_row = None
+        self.dendrogram_col = None
+
+    def dim_ratios(self, side_colors, axis, figsize):
+        """Get the proportions of the figure taken up by each axes
+        """
+        figdim = figsize[axis]
+
+        expected_size_for_dendrogram = self.expected_size_dendrogram  # Inches
+        expected_size_for_side_colors = self.expected_size_side_colors  # Inches
+
+        # Get resizing proportion of this figure for the dendrogram and
+        # colorbar, so only the heatmap gets bigger but the dendrogram stays
+        # the same size.
+        dendrogram = expected_size_for_dendrogram / figdim
+
+        # add the colorbar
+        colorbar_width = .8 * dendrogram
+        colorbar_height = .2 * dendrogram
+        if axis == 1:
+            ratios = [colorbar_width, colorbar_height]
+        else:
+            ratios = [colorbar_height, colorbar_width]
+
+        if side_colors is not None:
+            colors_shape = np.asarray(side_colors).shape
+            # This happens when a series or a list is passed
+            if len(colors_shape) <= 2:
+                n_colors = 1
+            # And this happens when a dataframe is passed, the first dimension is number of colors
+            else:
+                n_colors = colors_shape[0]
+
+            # Multiply side colors size by the number of colors
+            expected_size_for_side_colors = n_colors * expected_size_for_side_colors
+
+            side_colors_ratio = expected_size_for_side_colors / figdim
+
+            # Add room for the colors
+            ratios += [side_colors_ratio]
+
+        # Add the ratio for the heatmap itself
+        ratios.append(1 - sum(ratios))
+
+        return ratios
+
+
+def clustermap(data, pivot_kws=None, method='average', metric='euclidean',
+               z_score=None, standard_scale=None, figsize=None, cbar_kws=None,
+               row_cluster=True, col_cluster=True,
+               row_linkage=None, col_linkage=None,
+               row_colors=None, col_colors=None, mask=None,
+               expected_size_dendrogram=1.0,
+               expected_size_colors=0.25,
+               **kwargs):
+    """Plot a matrix dataset as a hierarchically-clustered heatmap.
+    Parameters
+    ----------
+    data: 2D array-like
+        Rectangular data for clustering. Cannot contain NAs.
+    pivot_kws : dict, optional
+        If `data` is a tidy dataframe, can provide keyword arguments for
+        pivot to create a rectangular dataframe.
+    method : str, optional
+        Linkage method to use for calculating clusters.
+        See scipy.cluster.hierarchy.linkage documentation for more information:
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.linkage.html
+    metric : str, optional
+        Distance metric to use for the data. See
+        scipy.spatial.distance.pdist documentation for more options
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.pdist.html
+        To use different metrics (or methods) for rows and columns, you may
+        construct each linkage matrix yourself and provide them as
+        {row,col}_linkage.
+    z_score : int or None, optional
+        Either 0 (rows) or 1 (columns). Whether or not to calculate z-scores
+        for the rows or the columns. Z scores are: z = (x - mean)/std, so
+        values in each row (column) will get the mean of the row (column)
+        subtracted, then divided by the standard deviation of the row (column).
+        This ensures that each row (column) has mean of 0 and variance of 1.
+    standard_scale : int or None, optional
+        Either 0 (rows) or 1 (columns). Whether or not to standardize that
+        dimension, meaning for each row or column, subtract the minimum and
+        divide each by its maximum.
+    figsize: tuple of two ints, optional
+        Size of the figure to create.
+    cbar_kws : dict, optional
+        Keyword arguments to pass to ``cbar_kws`` in ``heatmap``, e.g. to
+        add a label to the colorbar.
+    {row,col}_cluster : bool, optional
+        If True, cluster the {rows, columns}.
+    {row,col}_linkage : numpy.array, optional
+        Precomputed linkage matrix for the rows or columns. See
+        scipy.cluster.hierarchy.linkage for specific formats.
+    {row,col}_colors : list-like or pandas DataFrame/Series, optional
+        List of colors to label for either the rows or columns. Useful to
+        evaluate whether samples within a group are clustered together. Can
+        use nested lists or DataFrame for multiple color levels of labeling.
+        If given as a DataFrame or Series, labels for the colors are extracted
+        from the DataFrames column names or from the name of the Series.
+        DataFrame/Series colors are also matched to the data by their
+        index, ensuring colors are drawn in the correct order.
+    mask : boolean array or DataFrame, optional
+        If passed, data will not be shown in cells where ``mask`` is True.
+        Cells with missing values are automatically masked. Only used for
+        visualizing, not for calculating.
+    expected_size_dendrogram: float, optional
+        Size (in the same units as figsize) of the dendrogram size
+    expected_size_colors: float, optional
+        Size (in the same units as figsize) of size for row/col columns, if passed.
+    kwargs : other keyword arguments
+        All other keyword arguments are passed to ``sns.heatmap``
+    Returns
+    -------
+    clustergrid : ClusterGrid
+        A ClusterGrid instance.
+    Notes
+    -----
+    The returned object has a ``savefig`` method that should be used if you
+    want to save the figure object without clipping the dendrograms.
+    To access the reordered row indices, use:
+    ``clustergrid.dendrogram_row.reordered_ind``
+    Column indices, use:
+    ``clustergrid.dendrogram_col.reordered_ind``
+    Examples
+    --------
+    Plot a clustered heatmap:
+    .. plot::
+        :context: close-figs
+        >>> import seaborn as sns; sns.set(color_codes=True)
+        >>> iris = sns.load_dataset("iris")
+        >>> species = iris.pop("species")
+        >>> g = sns.clustermap(iris)
+    Use a different similarity metric:
+    .. plot::
+        :context: close-figs
+        >>> g = sns.clustermap(iris, metric="correlation")
+    Use a different clustering method:
+    .. plot::
+        :context: close-figs
+        >>> g = sns.clustermap(iris, method="single")
+    Use a different colormap and ignore outliers in colormap limits:
+    .. plot::
+        :context: close-figs
+        >>> g = sns.clustermap(iris, cmap="mako", robust=True)
+    Change the size of the figure:
+    .. plot::
+        :context: close-figs
+        >>> g = sns.clustermap(iris, figsize=(6, 7))
+    Plot one of the axes in its original organization:
+    .. plot::
+        :context: close-figs
+        >>> g = sns.clustermap(iris, col_cluster=False)
+    Add colored labels:
+    .. plot::
+        :context: close-figs
+        >>> lut = dict(zip(species.unique(), "rbg"))
+        >>> row_colors = species.map(lut)
+        >>> g = sns.clustermap(iris, row_colors=row_colors)
+    Standardize the data within the columns:
+    .. plot::
+        :context: close-figs
+        >>> g = sns.clustermap(iris, standard_scale=1)
+    Normalize the data within the rows:
+    .. plot::
+        :context: close-figs
+        >>> g = sns.clustermap(iris, z_score=0)
+    """
+    plotter = MyClusterGrid(data, pivot_kws=pivot_kws, figsize=figsize,
+                          row_colors=row_colors, col_colors=col_colors,
+                          z_score=z_score, standard_scale=standard_scale,
+                          mask=mask, expected_size_dendrogram=expected_size_dendrogram,
+                          expected_size_colors=expected_size_colors)
+
+    return plotter.plot(metric=metric, method=method,
+                        colorbar_kws=cbar_kws,
+                        row_cluster=row_cluster, col_cluster=col_cluster,
+                        row_linkage=row_linkage, col_linkage=col_linkage,
+                        **kwargs)
+
 
 def groupday_longform_factors_annotated_clusfolders(
         mice=['OA27', 'OA26', 'OA67', 'VF226', 'CC175'],
