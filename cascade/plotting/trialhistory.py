4efb044378e962a77b81def51ded6f710fdc50c6
Kelly McGuire <kellyleecemcguire@gmail.com>
2019-09-05 11:46:26 -0400

    added new trial history modules for plotting and summarizing data in pandas dfs

--- cascade/plotting/trialhistory.py
+++ cascade/plotting/trialhistory.py
@@ -10,17 +10,32 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 from .. import paths, load
+from .. import trialhistory as th
 from scipy.stats import pearsonr
 
 
 def groupmouse_index_heatmap(
-    mice=['OA27', 'OA26'],
-    words=['tray', 'obligations']
-    group_by='all'):
+        mice=['OA27', 'OA26', 'OA67', 'VF226', 'CC175', 'OA32', 'OA34', 'OA36'],
+        words=None,
+        group_by='all',
+        rank_num=18,
+        verbose=True):
 
-            
+    # get all dataframes
+    all_dfs = th.groupmouse_th_index_dataframe(
+                mice=mice,
+                words=words,
+                group_by=group_by,
+                rank_num=rank_num,
+                verbose=verbose)
+
+    # create colormap
     cmap = sns.diverging_palette(220, 10, sep=30, as_cmap=True)
-    sorter = np.argsort(trial_hist_mod[:, -1])
+
+    # sort according to degree of modulation by behavioral performance
+    # a.k.a., learning
+    sorter = np.argsort(all_dfs['learning_index'].values)
+
     # sorter = np.arange(0,rank)
     xlab = ['sensory_history', 'reward_history', 'reward_history - sensory_history', 'learning_idx']
     plt.figure()
