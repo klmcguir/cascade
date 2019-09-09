967893dd284654e48844bf1d624ec227308b2fd9
Kelly McGuire <kellyleecemcguire@gmail.com>
2019-09-09 15:45:57 -0400

    db name space

--- cascade/plotting/trialhistory.py
+++ cascade/plotting/trialhistory.py
@@ -70,18 +70,18 @@ def groupmouse_index_heatmap(
     plt_df = all_dfs.reset_index(['component']).values
 
     cs_to_check = ['plus', 'minus', 'neutral']
-    for cs in cs_to_check:
-        cs_bool = all_dfs.reset_index()['condition'].values == cs
+    for ics in cs_to_check:
+        cs_bool = all_dfs.reset_index()['condition'].values == ics
         sort_bool = cs_bool[sorter]
         cs_plt_df = plt_df[sorter, 1:][sort_bool]
         cs_y_label = plt_df[sorter, 0][sort_bool]
 
         plt.figure()
         group_word = paths.groupmouse_word({'mice': mice})
-        file_name = group_word + '_th_' + str(cs) + '.pdf'
+        file_name = group_word + '_th_' + str(ics) + '.pdf'
         sns.heatmap(cs_plt_df, center=0, vmax=1, vmin=-1, cmap=cmap,
                     yticklabels=cs_y_label, xticklabels=xlab)
-        plt.title('Trial History Modulation, Condition: ' + cs)
+        plt.title('Trial History Modulation, Condition: ' + ics)
         plt.ylabel('Component #')
         plt.savefig(save_dir + file_name, bbox_inches='tight')
         plt.close('all')
@@ -150,17 +150,17 @@ def groupday_index_heatmap(
         plt_df = all_dfs.reset_index(['component']).values
 
         cs_to_check = ['plus', 'minus', 'neutral']
-        for cs in cs_to_check:
-            cs_bool = all_dfs.reset_index()['condition'].values == cs
+        for ics in cs_to_check:
+            cs_bool = all_dfs.reset_index()['condition'].values == ics
             sort_bool = cs_bool[sorter]
             cs_plt_df = plt_df[sorter, 1:][sort_bool]
             cs_y_label = plt_df[sorter, 0][sort_bool]
 
             plt.figure()
-            file_name = m + '_th_' + str(cs) + '.pdf'
+            file_name = m + '_th_' + str(ics) + '.pdf'
             sns.heatmap(cs_plt_df, center=0, vmax=1, vmin=-1, cmap=cmap,
                         yticklabels=cs_y_label, xticklabels=xlab)
-            plt.title('Trial History Modulation, Condition: ' + cs)
+            plt.title('Trial History Modulation, Condition: ' + ics)
             plt.ylabel('Component #')
             plt.savefig(save_dir + file_name, bbox_inches='tight')
             plt.close('all')
