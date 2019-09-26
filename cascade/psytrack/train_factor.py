3778bcb4f9178c7637cfc90e17cb6f5e00441cb6
Kelly McGuire <kellyleecemcguire@gmail.com>
2019-09-25 23:05:11 -0400

    db sigmas

--- cascade/psytrack/train_factor.py
+++ cascade/psytrack/train_factor.py
@@ -97,8 +97,8 @@ def train(
         for ci in range(1, rank_num + 1):
             weights['factor_' + str(ci)] = 1
         # update sigmas
-        hyper['sigma'] = [2**-4.]*k
-        hyper['sigDay'] = [2**-4.]*k
+        hyper['sigma'] = [2**-4.]*rank_num
+        hyper['sigDay'] = [2**-4.]*rank_num
     else:
         # add 'y' but now it is a 1-2 binary vector of a TCA trial factor
         data = _splice_data_y(
