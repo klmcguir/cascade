3fd60967d35f7ed6c1dd4b7b8700d92d2717cfe0
Kelly McGuire <kellyleecemcguire@gmail.com>
2020-01-08 17:16:44 -0500

    db

--- cascade/plotting/adaptation.py
+++ cascade/plotting/adaptation.py
@@ -35,6 +35,7 @@ def weighted_avg_first100(
         rank=15,
         run_threshold=3,
         stim_window='last_sec', # 'first_sec' is the other option
+        func_type='no_offset'
         ):
 
     # load TCA models and data
