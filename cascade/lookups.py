9227a0e19e3b046a8f741a39dc66ba4a51c61f19
Kelly McGuire <kellyleecemcguire@gmail.com>
2020-04-02 12:09:15 -0400

    added blank to lookup

--- cascade/lookups.py
+++ cascade/lookups.py
@@ -7,18 +7,18 @@ color_dict = {'plus': [0.46, 0.85, 0.47, 1],
      'reversal': [173/255, 38/255, 26/255, 1],
      'gray': [163/255, 163/255, 163/255, 1]}
 
-lookup = {'OA27': {'plus': 270, 'minus': 135, 'neutral': 0},
-     'VF226': {'plus': 0, 'minus': 270, 'neutral': 135},
-     'OA67': {'plus': 0, 'minus': 270, 'neutral': 135},
-     'OA32': {'plus': 135, 'minus': 0, 'neutral': 270},
-     'OA34': {'plus': 270, 'minus': 135, 'neutral': 0},
-     'OA36': {'plus': 0, 'minus': 270, 'neutral': 135},
-     'OA26': {'plus': 270, 'minus': 135, 'neutral': 0}}
+lookup = {'OA27': {'plus': 270, 'minus': 135, 'neutral': 0, 'blank': -1},
+     'VF226': {'plus': 0, 'minus': 270, 'neutral': 135, 'blank': -1},
+     'OA67': {'plus': 0, 'minus': 270, 'neutral': 135, 'blank': -1},
+     'OA32': {'plus': 135, 'minus': 0, 'neutral': 270, 'blank': -1},
+     'OA34': {'plus': 270, 'minus': 135, 'neutral': 0, 'blank': -1},
+     'OA36': {'plus': 0, 'minus': 270, 'neutral': 135, 'blank': -1},
+     'OA26': {'plus': 270, 'minus': 135, 'neutral': 0, 'blank': -1}}
 
-lookup_ori = {'OA27': {270: 'plus', 135: 'minus', 0: 'neutral'},
-     'VF226': {0: 'plus', 270: 'minus', 135: 'neutral'},
-     'OA67': {0: 'plus', 270: 'minus', 135: 'neutral'},
-     'OA32': {135: 'plus', 0: 'minus', 270: 'neutral'},
-     'OA34': {270: 'plus', 135: 'minus', 0: 'neutral'},
-     'OA36': {0: 'plus', 270: 'minus', 135: 'neutral'},
-     'OA26': {270: 'plus', 135: 'minus', 0: 'neutral'}}
+lookup_ori = {'OA27': {270: 'plus', 135: 'minus', 0: 'neutral', -1: 'blank'},
+     'VF226': {0: 'plus', 270: 'minus', 135: 'neutral', -1: 'blank'},
+     'OA67': {0: 'plus', 270: 'minus', 135: 'neutral', -1: 'blank'},
+     'OA32': {135: 'plus', 0: 'minus', 270: 'neutral', -1: 'blank'},
+     'OA34': {270: 'plus', 135: 'minus', 0: 'neutral', -1: 'blank'},
+     'OA36': {0: 'plus', 270: 'minus', 135: 'neutral', -1: 'blank'},
+     'OA26': {270: 'plus', 135: 'minus', 0: 'neutral', -1: 'blank'}}
